99a50e0b6358dc0773c3f68fc0030666655ef95f
Ruan <ruandm49720@gmail.com>
2021-10-26 21:47:13 -0300

    validating

--- src/controller/matches.controller.js
+++ src/controller/matches.controller.js
@@ -3,21 +3,18 @@ const matchesService = require('../service/matches.service')
 const listAll = (req, res) => res.send(matchesService.listAll())
 
 const create = (req, res) =>{
-    matchesService.create(req.body)
-    res.send('created')
+    matchesService.create(req.body, res)
 }
 
 const remove = (req, res) =>{
     matchesService.remove(req.params.id, res)
-    res.send('deleted')
 }
 
-const listSpec = (req, res) => res.send(matchesService.listSpec(req.params.id))
+const listSpec = (req, res) => res.send(matchesService.listSpec(req.params.id, res))
 
 
 const update = (req, res)=>{
-    matchesService.update(req.params.id, req.body)
-    res.send('updated')
+    matchesService.update(req.params.id, req.body, res)
 }
 
 module.exports = {
--- src/service/matches.service.js
+++ src/service/matches.service.js
@@ -1,35 +1,65 @@
 const matchesDB = []
 let matchID = 1
 
-const create = match =>{
-    match.id = matchID ++
-    matchesDB.push(match)
+const create = (match, res) => {
+    if (valideCreate(match, res)) {
+        match.id = matchID++
+        matchesDB.push(match)
+        res.send('created')
+    }
+
 }
 
 const listAll = () => matchesDB
 
-const remove = (id, res) =>{
+const remove = (id, res) => {
     const matchIndex = matchesDB.findIndex(m => m.id == id)
-    if(matchIndex >=0){
-        matchesDB.splice(matchIndex, 1)
+    if (matchIndex >= 0) {
+        res.send(matchesDB.splice(matchIndex, 1))
+
     } else {
         res.statusCode = 400
+        res.send('id inválido')
     }
 }
 
-const update = (id, newMatch) =>{
+const update = (id, newMatch, res) => {
     const matchIndex = matchesDB.findIndex(m => m.id == id)
-    if(matchIndex >=0){
-        newMatch.id = matchID++
-        matchesDB[matchIndex] = newMatch
-    }
+    if (valideCreate(newMatch, res)) {
+        if (matchIndex >= 0) {
+            newMatch.id = matchID++
+            matchesDB[matchIndex] = newMatch
+            res.send('updated')
+        } else {
+            res.statusCode = 400
+            res.send('id inválido')
+        }
+    } 
+
 }
 
-const listSpec = id =>{
+const listSpec = (id, res) => {
     const matchIndex = matchesDB.findIndex(m => m.id == id)
-    if(matchIndex >= 0){
+    if (matchIndex >= 0) {
         return matchesDB[matchIndex]
+    } else {
+        res.statusCode = 400
+        res.send('id inválido')
     }
+}
+
+const valideCreate = (av1, res) => {
+    if (!av1.nome || av1.nome.trim() == '') {
+        res.statusCode = 400
+        res.send('Nome obrigatório')
+        return false
+    } else if (!av1.coach || av1.coach.trim() == '') {
+        res.statusCode = 400
+        res.send('Coach obrigatório')
+        return false
+    }
+    return true
+
 }
 
 module.exports = {
